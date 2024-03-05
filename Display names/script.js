7dcab4687bfa0f22a80bd55cf6233acde3e17f23
vishavjeet2024 <162081910+vishavjeet2024@users.noreply.github.com>
2024-03-05 21:53:03 +0530

    Add files via upload

---
+++ Display names/script.js
@@ -0,0 +1,9 @@
+// Define the names
+const A = "John";
+const B = "Eliana";
+
+// Construct the message using template literals
+const message = 'A says Hi to B';
+
+// Print the message to the console
+console.log(message);
