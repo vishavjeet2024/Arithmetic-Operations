905a93c68b54f7b7f938d248f3f94e6c89aa6d92
vishavjeet2024 <162081910+vishavjeet2024@users.noreply.github.com>
2024-03-05 21:53:46 +0530

    Add files via upload

---
+++ Rest Operator/script.js
@@ -0,0 +1,15 @@
+// object
+const person = {
+	name: "vishavjeet";
+	city: "patiala";
+	jobTitle: "Web designer";
+};
+
+// creating copy
+
+const updatedPerson = {...person, currentLocation: "mohali"};
+
+// print
+
+console.log("updatedPerson");
+console.log("person");
